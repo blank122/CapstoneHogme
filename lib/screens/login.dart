47c8ff558f46f194e5a223482473eb3f8cb5b810
Joshua De Vera <deverajoshua02@gmail.com>
2023-08-22 11:14:24 +0800

    minor changes on login.dart

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -33,115 +33,118 @@ class _LoginState extends State<Login> {
     return SafeArea(
       child: Scaffold(
         backgroundColor: AppTheme.primaryBackground,
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            //logo with text
+        body: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              //logo with text
 
-            Row(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(left: 15.00, right: 15.00),
-                  child: Image.asset(
-                    "assets/icons/icon_hogme.png",
-                    height: 100,
-                    width: 100,
+              Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 15.00, right: 15.00),
+                    child: Image.asset(
+                      "assets/icons/icon_hogme.png",
+                      height: 100,
+                      width: 100,
+                    ),
                   ),
-                ),
-                text50BoldHeading(text: 'HOGME'),
-              ],
-            ),
+                  text50BoldHeading(text: 'HOGME'),
+                ],
+              ),
 
-            //slogan - nurturing livestocks with innovation
-            Padding(
-              padding: const EdgeInsets.only(top: 10.00, bottom: 20.00),
-              child: text16Bold(text: "Nurturing livestocks with innovation"),
-            ),
-            //email placeholder with icons
-            Padding(
-              padding:
-                  const EdgeInsets.symmetric(horizontal: 25.0, vertical: 15),
-              child: TextFormField(
-                controller: usernamecontroller,
-                style: const TextStyle(color: Colors.black),
-                decoration: InputDecoration(
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12),
+              //slogan - nurturing livestocks with innovation
+              Padding(
+                padding: const EdgeInsets.only(top: 10.00, bottom: 20.00),
+                child: text16Bold(text: "Nurturing livestocks with innovation"),
+              ),
+              //email placeholder with icons
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 25.0, vertical: 15),
+                child: TextFormField(
+                  controller: usernamecontroller,
+                  style: const TextStyle(color: Colors.black),
+                  decoration: InputDecoration(
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    prefixIcon: const Icon(
+                      Icons.email_outlined,
+                      color: Colors.black,
+                    ),
+                    labelText: "Email Address",
+                    labelStyle: const TextStyle(color: Colors.black),
+                    border: InputBorder.none,
+                    filled: true,
                   ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  prefixIcon: const Icon(
-                    Icons.email_outlined,
-                    color: Colors.black,
-                  ),
-                  labelText: "Email Address",
-                  labelStyle: const TextStyle(color: Colors.black),
-                  border: InputBorder.none,
-                  filled: true,
                 ),
               ),
-            ),
-            //password with icons
-            Padding(
-              padding:
-                  const EdgeInsets.symmetric(horizontal: 25.0, vertical: 15),
-              child: TextFormField(
-                controller: passwordcontroller,
-                obscureText: true,
-                style: const TextStyle(color: Colors.black),
-                decoration: InputDecoration(
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12),
+              //password with icons
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 25.0, vertical: 15),
+                child: TextFormField(
+                  controller: passwordcontroller,
+                  obscureText: true,
+                  style: const TextStyle(color: Colors.black),
+                  decoration: InputDecoration(
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    prefixIcon: const Icon(
+                      Icons.password,
+                      color: Colors.black,
+                    ),
+                    labelText: "Password",
+                    labelStyle: const TextStyle(color: Colors.black),
+                    border: InputBorder.none,
+                    filled: true,
                   ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  prefixIcon: const Icon(
-                    Icons.password,
-                    color: Colors.black,
-                  ),
-                  labelText: "Password",
-                  labelStyle: const TextStyle(color: Colors.black),
-                  border: InputBorder.none,
-                  filled: true,
                 ),
               ),
-            ),
-            //forgot password = if makaya
-            Container(
-              margin: const EdgeInsets.only(left: 25.00),
-              child: textUnderline(text: "Forgot password?"),
-            ),
-            //login button
-            GestureDetector(
-              onTap: () => {print("tapped")},
-              child: Container(
-                width: 325,
-                height: 50,
-                margin: const EdgeInsets.only(top: 25, left: 25, right: 25),
-                decoration: buttonShadow(themeColor: AppTheme.primaryButtons),
-                child: Center(
-                  child: text16Bold(text: "Log in", defaultColor: Colors.white),
+              //forgot password = if makaya
+              Container(
+                margin: const EdgeInsets.only(left: 25.00),
+                child: textUnderline(text: "Forgot password?"),
+              ),
+              //login button
+              GestureDetector(
+                onTap: () => {print("tapped")},
+                child: Container(
+                  width: 325,
+                  height: 50,
+                  margin: const EdgeInsets.only(top: 25, left: 25, right: 25),
+                  decoration: buttonShadow(themeColor: AppTheme.primaryButtons),
+                  child: Center(
+                    child:
+                        text16Bold(text: "Log in", defaultColor: Colors.white),
+                  ),
                 ),
               ),
-            ),
-            const Row(),
-            //Create account button
-            GestureDetector(
-              onTap: () => {},
-              child: Container(
-                width: 325,
-                height: 50,
-                margin: const EdgeInsets.only(top: 25, left: 25, right: 25),
-                decoration: buttonShadow(themeColor: Colors.white),
-                child: Center(
-                  child:
-                      text16Bold(text: "Sign up", defaultColor: Colors.black),
+              const Row(),
+              //Create account button
+              GestureDetector(
+                onTap: () => {},
+                child: Container(
+                  width: 325,
+                  height: 50,
+                  margin: const EdgeInsets.only(top: 25, left: 25, right: 25),
+                  decoration: buttonShadow(themeColor: Colors.white),
+                  child: Center(
+                    child:
+                        text16Bold(text: "Sign up", defaultColor: Colors.black),
+                  ),
                 ),
-              ),
-            )
-          ],
+              )
+            ],
+          ),
         ),
       ),
     );
